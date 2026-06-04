746d58ac477b40a3583966958d1d4a54cc84e996
Eduardo Pelizzari de Andrade <eduardoandrade@persoft.com.br>
2026-06-04 20:12:21 -0300

    fonts

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ sudo apt install tmux -y
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-cp ~/configDesktop/* ~/ -r
+cp -a .[^.]* ~/ -r
