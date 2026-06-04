9aefebabe01c7397ab48aca86a041e0bdae5e7ba
Eduardo Pelizzari de Andrade <eduardoandrade@persoft.com.br>
2026-06-04 17:30:19 -0300

    ajuste na instalação do neovim

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@ sudo apt install nodejs -y
 sudo apt install npm -y
 sudo apt install flamerobin -y
 sudo apt install tmux -y
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 cp ~/configDesktop/* ~/ -r
