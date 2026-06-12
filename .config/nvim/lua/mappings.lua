b9c864f27d2b1f81d39bfbb17fcdfe34595edfe9
Eduardo Pelizzari de Andrade <eduardoandrade@persoft.com.br>
2026-06-11 22:16:43 -0300

    salvando dados

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,5 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>tf", "<cmd>NvimTreeFindFile<CR>", { desc = "NvimTree focar arquivo atual" })
 map("n", "<leader>py", "<cmd>w | !python3 %<cr>", { desc = "Python: Execute script" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ sudo apt install nodejs -y
 sudo apt install npm -y
 sudo apt install flamerobin -y
 sudo apt install tmux -y
+sudo apt install wl-clipoard -y
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.telegram.desktop
+flatpak install --user flathub org.remmina.Remmina
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
